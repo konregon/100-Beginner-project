8d7a8f6acd1cb01f845164e828d5b7273fc0df8f
MAULIK RAJENDRA PATEL <maulikpatelmow@gmail.com>
2022-08-22 19:09:41 +0530

    Update index.js

--- 28. Hangman (spelling) Game/index.js
+++ 28. Hangman (spelling) Game/index.js
@@ -105,3 +105,116 @@ function createTastur() {
     }
 }
 
+// Game check, If show next error / game end
+function bTas(a) {
+    if(a.getAttribute("data") == "") {
+        var x = isExist(a.innerText)
+        a.setAttribute("data", x)
+        if(x) {
+            if(wordLeft.length == 0) {
+                gameEnd(true)
+            }
+        } else {
+            showNextFail()
+        }
+    }
+}
+
+// If letter "X" exist
+function isExist(e) {
+    e = e.toUpperCase()
+    var x = wordLeft.indexOf(e)
+    if(x != -1) {
+        wordLeft.splice(x, 1)
+        typeWord(e)
+        return true
+    }
+    return false
+}
+
+// Show next fail drawing
+function showNextFail() {
+    fail++
+    switch(fail) {
+        case 1:
+            gId("g0").setAttribute("data", "true")
+            break;
+        
+        case 2:
+            gId("g1").setAttribute("data", "true")
+            break;
+        
+        case 3:
+            gId("g2").setAttribute("data", "true")
+            break;
+        
+        case 4:
+            gId("g3").setAttribute("data", "true")
+            gId("hintButton").setAttribute("data", "true")
+            break;
+        
+        case 5:
+            gId("g4").setAttribute("data", "true")
+            break;
+        
+        case 6:
+            gId("g5").setAttribute("data", "true")
+            break;
+        
+        case 7:
+            gId("g5").setAttribute("l", "true")
+            break;
+        
+        case 8:
+            gId("g5").setAttribute("r", "true")
+            break;
+        
+        case 9:
+            gId("g6").setAttribute("data", "true")
+            gId("g6").setAttribute("l", "true")
+            break;
+        
+        case 10:
+            gId("g6").setAttribute("r", "true")
+            gameEnd(false)
+            break;
+    }
+}
+
+function typeWord(e) {
+    for(a = 0; a < word[select][0].length; a++) {
+        if(word[select][0][a].toUpperCase() == e) {
+            gId("l" + a).innerText = e
+        }
+    }
+}
+
+// Game result
+function gameEnd(e) {
+    var d = gId("result")
+    d.setAttribute("data", e)
+    if(e) {
+        gId("rT").innerText = "You Win!"
+        gId("rM").innerHTML = "Congratulations, you found the word!<br/><br/>Good Job!"
+    } else {
+        gId("rT").innerText = "You Lose!"
+        gId("rM").innerHTML = "The word was <br/><br/>\"" + word[select][0].toUpperCase() + "\"<br/><br/>Better luck next time."
+    }
+    d.className = ""
+}
+
+// Show hint
+function hint() {
+    gId("hintText").innerText = word[select][1]
+    gId("hint").style.display = "block"
+}
+
+// Exit hint
+function hintExit() {
+    gId("hint").style.display = "none"
+}
+
+// Get HTML ID element by name
+function gId(a) {
+    return document.getElementById(a)
+}
